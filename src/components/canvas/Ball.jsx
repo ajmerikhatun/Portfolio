106521654d5dd4f41e144a98ac4c19112470c2df
Ajmeri Khatun <134210240+ajmerikhatun@users.noreply.github.com>
2024-08-12 21:53:16 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -4,6 +4,8 @@ import {
   Decal, Float, OrbitControls, Preload, useTexture
 } from '@react-three/drei'
 
+import Loader from '../Loader';
+
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
